c544a614ad9c370e3de03ca58e33fdc7757cb2f5
Jogeonsang <rhcn258@gmail.com>
2020-06-17 16:29:04 +0900

    TS interface Practice

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
-const name:string = "marcus",
-    age:number = 23,
-    gender:string = 'male';
-const sayHi = (name: string, age: number, gender: string):void => {
+interface Person {
+    name: string,
+    gender: string,
+    age: number,
+};
+
+const person = {
+    name: 'marcus',
+    gender: 'male',
+    age: 23
+};
+
+const sayHi = (person:Person):void => {
+    const {name, age, gender} = person;
     console.log(`Hello ${name}, You are ${age}, you are a ${gender}`);
 };
 
-sayHi(name, age, gender);
+sayHi(person);
 
 export {}
